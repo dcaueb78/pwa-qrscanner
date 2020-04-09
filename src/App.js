0db5acb1962b88915e496fbfbd9422998b359bf8
Cauê Kotarski <caue1017@hotmail.com>
2020-04-09 17:40:36 -0300

    Create qr scanner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import QrReader from "react-qr-reader";
 
 function App() {
+  const QRCodeDelay = 0;
+  const [message, setMessage] = useState("oioi");
+
+  function handleScan(QRCodeScannedMessage) {
+    if (QRCodeScannedMessage) {
+      setMessage(QRCodeScannedMessage);
+      console.log(QRCodeScannedMessage);
+    }
+  }
+
+  function handleQRCodeScanError() {
+    // toast.error(
+    //   "Houve um problema na leitura do código QR, poderia tentar novamente?"
+    // );
+    console.log("oi");
+  }
+
+  const previewStyle = {
+    height: 100,
+    width: "100%",
+    margin: 15,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <QrReader
+          resolution={1920}
+          facingMode="environment"
+          delay={QRCodeDelay}
+          style={previewStyle}
+          onError={handleQRCodeScanError}
+          onScan={handleScan}
+          className="QRcodeScaner"
+        />
+        <h1>{message}</h1>
+      </div>
     </div>
   );
 }
